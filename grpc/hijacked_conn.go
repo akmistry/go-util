78eec6a87e937ecf2e0ed1b3ba6a4be504b33096
Anand K. Mistry <anand@mistry.ninja>
2018-02-19 16:11:25 +1100

    Simplify hijacked conn.

--- grpc/hijacked_conn.go
+++ grpc/hijacked_conn.go
@@ -3,14 +3,11 @@ package grpc
 import (
 	"bufio"
 	"net"
-	"sync"
-	"time"
 )
 
 type hijackedConn struct {
-	conn net.Conn
+	net.Conn
 
-	lock sync.Mutex
 	bufr *bufio.Reader
 }
 
@@ -27,51 +24,22 @@ func newHijackedConn(conn net.Conn, bufr *bufio.Reader, bufw *bufio.Writer) (_ *
 		}
 	}
 
-	return &hijackedConn{conn: conn, bufr: bufr}, nil
+	return &hijackedConn{Conn: conn, bufr: bufr}, nil
 }
 
 func (c *hijackedConn) Read(b []byte) (n int, err error) {
-	c.lock.Lock()
 	if c.bufr != nil {
 		if c.bufr.Buffered() > 0 {
-			defer c.lock.Unlock()
 			readLen := len(b)
 			if readLen > c.bufr.Buffered() {
+				// Don't read more than the buffered data to prevent bufr from re-filling
+				// the buffer.
 				readLen = c.bufr.Buffered()
 			}
 			return c.bufr.Read(b[:readLen])
 		}
 		c.bufr = nil
 	}
-	c.lock.Unlock()
 
-	return c.conn.Read(b)
-}
-
-func (c *hijackedConn) Write(b []byte) (n int, err error) {
-	return c.conn.Write(b)
-}
-
-func (c *hijackedConn) Close() error {
-	return c.conn.Close()
-}
-
-func (c *hijackedConn) LocalAddr() net.Addr {
-	return c.conn.LocalAddr()
-}
-
-func (c *hijackedConn) RemoteAddr() net.Addr {
-	return c.conn.RemoteAddr()
-}
-
-func (c *hijackedConn) SetDeadline(t time.Time) error {
-	return c.conn.SetDeadline(t)
-}
-
-func (c *hijackedConn) SetReadDeadline(t time.Time) error {
-	return c.conn.SetReadDeadline(t)
-}
-
-func (c *hijackedConn) SetWriteDeadline(t time.Time) error {
-	return c.conn.SetWriteDeadline(t)
+	return c.Conn.Read(b)
 }
